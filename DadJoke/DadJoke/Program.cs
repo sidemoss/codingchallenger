51d5c53c1220f7702d993276ce0d40bd393271bc
sidemoss <sidemoss@hotmail.com>
2020-02-13 13:18:34 -0700

    Adding initial code.

---
+++ DadJoke/DadJoke/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadJoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For a list of dad jokes, enter a search term and press enter.\n" +
+                "For a random joke, just press enter.");
+            string searchString = Console.ReadLine();
+            JokeController.RunAsync(searchString);
+        }
+    }
+}
